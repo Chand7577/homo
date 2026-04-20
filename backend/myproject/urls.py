2d50f4b992ad86c9cdcfd6426f9b3d9d3f0a9fd5
chand7577 <amritchand0713@gmail.com>
2026-04-20 12:52:27 +0530

    Fix: Enable admin URL path

--- backend/myproject/urls.py
+++ backend/myproject/urls.py
@@ -1,10 +1,13 @@
+from django.contrib import admin
 from django.urls import path, include
 from homeopathy.views import HomeView
 from django.http import JsonResponse
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('test-cors/', lambda r: JsonResponse({'status': 'ok', 'message': 'CORS is working!'})),
     path('homeopathy/', include('homeopathy.urls')),
     path('', HomeView.as_view(), name='home'),
 ]
+
     
